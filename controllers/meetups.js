6ba975007c3879892e4617e7e7e5f20ff7599096
kossy360 <kossyugochukwu@gmail.com>
2019-01-16 11:20:54 +0100

    [chore] connected all meetup routes to database

--- controllers/meetups.js
+++ controllers/meetups.js
@@ -1,59 +1,67 @@
-import storage from '../storage';
 import validator from '../middleware/validator';
 import error from '../middleware/errorhandler';
+import db from '../db/db';
+import generate from '../db/querygenerator';
 
 const success = (status, data) => ({ status, data });
 
-const reduce = (meets) => {
-  const reducedMeet = meets.map((meet) => {
-    const result = {
-      id: meet.id,
-      location: meet.location,
-      happeningOn: meet.happeningOn,
-      topic: meet.topic,
-      images: meet.images,
-      tags: meet.tags,
-    };
-    return result;
-  });
-  return reducedMeet;
-};
+const fields = 'id, topic, location, happening, tags, images';
 
 const control = {
-  getAll: (req, res) => {
-    const meets = storage.meetups;
-    if (meets) res.status(200).send(success(200, reduce(meets)));
-    else {
-      res.status(200).send({
-        status: 200,
-        message: 'there are no meetup records available',
-      });
+  getAll: async (req, res) => {
+    try {
+      const { rows, rowCount } = await db.query(`SELECT * from all_meets_${req.decoded.isAdmin ? 'admin' : 'user'}(${req.decoded.user})`);
+      if (rowCount > 0) res.status(200).send(success(200, rows));
+      else {
+        res.status(200).send({
+          status: 200,
+          message: 'there are no meetup records available',
+        });
+      }
+    } catch (e) {
+      error(500, res);
     }
   },
 
-  getUpcoming: (req, res) => {
-    const meets = storage.meetups.filter(meet => meet.happeningOn > Date.now());
-    if (meets.length > 0) res.status(200).send(success(200, reduce(meets)));
-    else {
-      res.status(200).send({
-        status: 200,
-        message: 'there are no upcoming meetups',
-      });
+  getUpcoming: async (req, res) => {
+    try {
+      const { rows, rowCount } = await db.query(`SELECT * FROM all_meets_${req.decoded.isAdmin ? 'admin' : 'user'}(${req.decoded.user}) WHERE happening > ${Date.now() - 172800000}`);
+      if (rowCount > 0) res.status(200).send(success(200, rows));
+      else {
+        res.status(200).send({
+          status: 200,
+          message: 'there are no upcoming meetups',
+        });
+      }
+    } catch (e) {
+      error(500, res);
     }
   },
 
-  getSpecific: (req, res, next) => {
-    const meetup = storage.meetups.find(meet => meet.id.toString() === req.params.meetupId);
-    if (meetup) res.status(200).send(success(200, reduce([meetup])));
-    else next(404);
+  getSpecific: async (req, res, next) => {
+    try {
+      const { meetupId } = await validator(req.params, 'reqId');
+      const { rows, rowCount } = await db.query(`SELECT * FROM all_meets_${req.decoded.isAdmin ? 'admin' : 'user'}(${req.decoded.user}) WHERE id = $1`, [meetupId]);
+      if (rowCount > 0) res.status(200).send(success(200, rows));
+      else next(404);
+    } catch (e) {
+      error(400, res, e.details[0].message.replace(/"/g, ''));
+    }
   },
 
-  createNew: async (req, res) => {
-    const body = await validator(req.body, 'meetup').catch(e => error(400, res, e.details[0].message.replace(/"/g, '')));
-    if (!body) return;
-    body.id = `${storage.meetups.length + 1}`;
-    storage.meetups.push(body);
-    res.status(201).json(success(201, reduce([body])));
+  createNew: async (req, res, next) => {
+    if (req.decoded.isAdmin) {
+      try {
+        const body = await validator(req.body, 'meetup');
+        const { key1, key2, values } = generate.insertFields(body);
+        const { rows, rowCount } = await db.query(`INSERT INTO public.meets (${key1}) VALUES (${key2}) RETURNING ${fields}`, values);
+        if (rowCount > 0) res.status(201).json(success(201, rows));
+        else next(500);
+      } catch (e) {
+        if (e.details[0]) error(400, res, e.details[0].message.replace(/"/g, ''));
+        else error(500, res);
+      }
+    } else error(403, res, 'only users with administrative rights can create meetups');
   },
 };
 
