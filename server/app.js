84151f5f0e6d5d2054af665187dd58e195b5007c
kossy360 <kossyugochukwu@gmail.com>
2019-01-21 01:36:08 +0100

    [chore #163341458] remove app wide jwt verification, code refactoring

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ import cors from 'cors';
 
 import createError from './helpers/createError';
 import routes from './routes/indexRouter';
-import authenticator from './middleware/authenticator';
 
 const app = express();
 
@@ -17,8 +16,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1', (req, res, next) => {
-  if (req.path === '/') {
+app.use('/', (req, res, next) => {
+  if (req.path === '/' || /^\/api\/v\d+\/*$/.test(req.path)) {
     res.status(200).json({
       status: 200,
       message: 'welcome to questioner, please refer to our API docs for valid requests',
@@ -26,17 +25,9 @@ app.use('/api/v1', (req, res, next) => {
   } else next();
 });
 
-app.use('/api/v1/auth', routes.userRoute);
-app.use('/api/v1', [authenticator.verify, routes.generalRoute]);
+app.use('/api/v1', routes.userRoute, routes.generalRoute);
 
 app.use('/:invalid', (req, res) => createError(400, res, 'request path invalid, please refer to API documentation'));
-
-app.use('/', (req, res) => {
-  res.status(200).json({
-    status: 200,
-    message: 'welcome to questioner, please refer to our API docs for valid requests',
-  });
-});
 
 // error handler
 app.use((err, req, res, next) => {
